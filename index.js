6314ee63b1d733d6e33681e984521709f769f2ba
figueroar <raul.a.figueroa@hotmail.com>
2020-01-19 19:14:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const invited = ['Lisa', 'Kaitlin', 'Jan'];
+const newArray = [];
+let messages;
+function writeCards(invited) {
+  for (let i = 0; i < invited.length; i++) {
+    newArray.push(`Thank you, ${invited[i]}, for the wonderful surprise gift!`);
+  }
+  return newArray; 
+}
+
+function countDown( number ) {
+  while ( number > 0) {
+    console.log(number);
+      number -= 1;
+  }
+  console.log( number);
+}
+
