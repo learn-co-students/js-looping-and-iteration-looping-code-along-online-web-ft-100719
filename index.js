d9775121e2fb9b087aad6bd4e005a1f75938a9b9
Kathleen Kamali <kathleenkamali@gmail.com>
2020-01-21 12:31:42 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names) {
+  let cards = [];
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return cards;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
