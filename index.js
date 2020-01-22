06f2e1272011cfca1e2325cd026cd46c605c361b
rilemoore <rilemoore@gmail.com>
2020-01-22 08:52:30 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, name)
+{
+    let array = [];
+    for(let i = 0; i < names.length; i++)
+    {
+        array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return array
+}
+
+function countDown(number)
+{
+    while (number > -1)
+    {
+        console.log(number);
+        number --;
+    }
+}
