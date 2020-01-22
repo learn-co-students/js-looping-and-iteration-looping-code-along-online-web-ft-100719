86757a8b5d8b45dc8de73d12a04d816ff5585849
Jnorman2123 <Jnorman2123@gmail.com>
2020-01-21 20:10:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let thank_you_messages = [];
+
+function writeCards(people, message) {
+    for (let i = 0; i < people.length; i++) {
+      thank_you_messages.push (`Thank you, ${people[i]}, for the wonderful ${message} gift!`);
+     }
+     return thank_you_messages;
+}
+
+function countDown(int) { 
+    while (int >= 0) {
+        console.log(int); 
+        int -= 1;
+    }
+}
