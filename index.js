8c5ac2769568c47bdaa3eb6c747b23d44bb4e577
github username <github email address>
2020-01-22 15:20:03 -0600

    save progress

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const cards = (['Lisa', 'Kaitlin', 'Jan'], 'surprise');
+
+function writeCards(cards)  {
+    const messages = [ ]
+    for (let i = 0; i < cards.length; i++) {
+        const message = (`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+        messages.push(message);
+    }
+    return messages; 
+}
+
+let countDown = 10;
+while (countDown <= 10 ) {
+    console.log(countDown--);
+}
