41c5ea4ad2de04b60aa3cba8c1408b07a9bdf9c7
lmjcbs <hello@liamjacobs.dev>
2020-01-24 13:01:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames) {
+  const cardsArray = []; 
+  arrayOfNames.forEach(name => {
+    cardsArray.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+  });
+  return cardsArray;
+};
+
+function countDown(startingNum) {
+  for (let i = startingNum; i >= 0; i--) {
+    console.log(i);
+  }
+}
