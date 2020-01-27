c13d9703feb3f41b1d8ccdb9ab116b2b0af2e01c
Jesse <jesse.lee.smith@gmail.com>
2020-01-26 22:51:40 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(startNumber) {
+  for (let i = startNumber; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+function writeCards(names, event) {
+  let arrayToReturn = [];
+  for (let i = 0; i < names.length; i++) {
+    arrayToReturn.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return arrayToReturn;
+}
